f7ab0f6df49ee90f810dd3d20b28fe756de045e2
J O E <54571312+joece035@users.noreply.github.com>
2026-09-15 04:00:30 +0700

    update shared/personal/find_stuff.sh

--- shared/personal/find_stuff.sh
+++ shared/personal/find_stuff.sh
@@ -270,13 +270,11 @@ replace_w() {
             ;;
     esac
 }
-<<<<<<< HEAD
->>>>>>> 399b296 (fix vault)
-=======
+
 
 fword() {
     local word="${1:?"Please provide a word to search for"}"
     shift
     grep -rnii "$word" "${@:-.}:?"
 }
->>>>>>> bfc64d4 (last)
+
