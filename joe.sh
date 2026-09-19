7626d1e3c77ef75d655f42f9587a5eae1f386198
J O E <54571312+joece035@users.noreply.github.com>
2026-09-20 05:33:34 +0700

    update joe.sh

--- joe.sh
+++ joe.sh
@@ -178,7 +178,7 @@ auto_start_ssh(){
     fi
 	else
 		if ! pgrep -f "sshd.*-p.*${SSH_PORT}" >/dev/null 2>&1; then
-        $(command -v sshd -p "$SSH_PORT") && { command -v cn &>/dev/null && cn 10 b "SSH Daemon started on port ${SSH_PORT}." >&2; } || { command -v cn &>/dev/null && cn 9 b "Failed to start sshd on port ${SSH_PORT}." ; }
+        /usr/sbin/sshd -p "$SSH_PORT" && { command -v cn &>/dev/null && cn 10 b "SSH Daemon started on port ${SSH_PORT}." >&2; } || { command -v cn &>/dev/null && cn 9 b "Failed to start sshd on port ${SSH_PORT}." ; }
     else
         command -v cn &>/dev/null && cn 10 bi "ssh activated port : ${SSH_PORT}" #>&2	
 		fi		
